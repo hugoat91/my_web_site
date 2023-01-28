7a8d75dc25c2f3a3ca9f6282512b9996ff8a177e
Lacour Hugo <hugoat@MacBook-Pro-de-Lacour.local>
2023-01-28 20:21:36 +0900

    avancée site

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import AppliEsme from '../assets/portfolio/projet_esme_logo.jpg';
 import SiteWeb from '../assets/portfolio/website.jpg';
+import Biotech from '../assets/portfolio/biotech.png';
 
 const Portfolio = () => {
 
     const portfolios = [
         {
             id: 1,
-            src: AppliEsme
+            src: AppliEsme,
+            type: 'Demo'
         },
         {
             id: 2,
-            src: SiteWeb
+            src: SiteWeb,
+            type: 'Demo'
+        },
+        {
+            id: 3,
+            src: Biotech,
+            type: 'PowerPoint'
         },
 
 
@@ -36,7 +44,7 @@ const Portfolio = () => {
 
             <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
             {
-                portfolios.map(({id, src}) => (
+                portfolios.map(({id, src, type}) => (
                     <div key={id} className=' shadow-md shadow-gray-600 rounded-lg'>
                         <img 
                             src={src} 
@@ -45,10 +53,10 @@ const Portfolio = () => {
                         />
                         <div className=' flex items-center justify-center'>
                             <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                                Demo
+                                {type}
                             </button>
                             <button className=' w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>
-                                Code
+                                GitHub
                             </button>
                         </div>
                     </div>
